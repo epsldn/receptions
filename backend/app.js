e2a1a2bacf4ccb76f1fe7f13770ad62bd76d1047
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2023-03-13 19:14:02 -0500

    Add error handler

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,10 @@ require("express-async-errors");
 const morgan = require("morgan");
 const cors = require("cors");
 const csurf = require("csurf");
-const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 const routes = require("./routes");
+const { ValidationError } = require("sequelize");
 
 const environment = require("./config");
 const isProduction = environment == "production";
@@ -36,5 +37,38 @@ app.use(
 );
 
 app.use(routes);
+
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource could not be found.");
+    err.title = "Resource not found";
+    err.errors = { message: "The requested resource couldn't be found" };
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors) {
+            errors[errors.path] = error.message;
+        }
+        err.title = "Validation Error";
+        err.errors = errors;
+    }
+
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || "Server Error",
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
