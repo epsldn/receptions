ee0a7604dba1d1bd81f0badc54ec0653a2138a94
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2023-03-23 19:43:41 -0500

    Import needed functionality into frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { ReactDOM } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
