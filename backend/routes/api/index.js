06d53de488e50be39b85be1f74a0269ffbb32180
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2023-03-26 18:05:42 -0500

    Finish setting up xsrf

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -14,9 +14,9 @@ router.use(restoreUser);
 router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 
-router.post("/test", (req, res) => {
-    res.json({ requestBody: req.body });
-});
+// router.post("/test", (req, res) => {
+//     res.json({ requestBody: req.body });
+// });
 // router.get("/restore-user", (req, res) => {
 //     console.dir(req);
 //     return res.json(req.user);
--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -1,16 +1,20 @@
 import Cookies from "js-cookie";
 
-async function csrfFetch(url, options = {}) {
-    if (!options.header) options.header = {};
+export async function csrfFetch(url, options = {}) {
+    if (!options.headers) options.headers = {};
     if (!options.method) options.method = "GET";
 
     if (options.method.toUpperCase() !== "GET") {
         if (!options.headers["Content-Type"]) options.headers["Content-Type"] = "application/json";
-        options.headers["XSRF-Token"] = Cookies.get("XSRF-Token");
+        options.headers["XSRF-Token"] = Cookies.get("XSRF-TOKEN");
     }
 
     const response = await window.fetch(url, options);
 
     if (response.status > 400) throw response;
     else return response;
+}
+
+export function restoreCSRF() {
+    return csrfFetch("/api/csrf/restore");
 }
